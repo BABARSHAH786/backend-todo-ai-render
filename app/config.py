5c2e65a32a137bdf2169dbe6728604587d58fb5e
BABRSHAH786 <ba2091247@gmail.com>
2026-01-20 02:04:27 +0500

    update cors localhost 3000 allow

--- app/config.py
+++ app/config.py
@@ -7,8 +7,11 @@ class Settings(BaseSettings):
 
     database_url: str
     better_auth_secret: str
-    frontend_url: str = "https://todoagent.techkl.de"
-    cors_origins: str = "https://todoagent.techkl.de"
+    # frontend_url: str = "https://todoagent.techkl.de"
+    # frontend_url: str = "https://todoagent.techkl.de"  #comment due to check
+    cors_origins: str = "http://localhost:3000"
+    cors_origins: str = "http://localhost:3000"
+
 
     # Gemini Configuration
     gemini_api_key: str | None = None
